3d93563a35884a59185fa9eb3c4f58a07dda6419
Chuzzy <thechuzzyguy@gmail.com>
2020-12-01 11:46:17 +0000

    A lil' break statement never hurt nobody

--- 01/day01.mjs
+++ 01/day01.mjs
@@ -2,24 +2,30 @@ import { readFile } from "fs";
 readFile("01/input.txt", (err, data) => {
     if (err) throw err;
     let input = data.toString().split("\n").map(n => parseInt(n));
-    
+
     console.log("First half: ");
-    for (const a of input) {
-        for (const b of input) {
-            if (a + b == 2020) {
-                console.log("The answer is " + a + " and " + b);
-                console.log("The product of these is " + (a * b));
+    first: {
+        for (const a of input) {
+            for (const b of input) {
+                if (a + b == 2020) {
+                    console.log("The answer is " + a + " and " + b);
+                    console.log("The product of these is " + (a * b));
+                    break first;
+                }
             }
         }
     }
 
     console.log("Second half: ");
-    for (const a of input) {
-        for (const b of input) {
-            for (const c of input) {
-                if (a + b + c == 2020) {
-                    console.log("The answer is " + a + " and " + b + " and " + c);
-                    console.log("The product of these is " + (a * b * c));
+    second: {
+        for (const a of input) {
+            for (const b of input) {
+                for (const c of input) {
+                    if (a + b + c == 2020) {
+                        console.log("The answer is " + a + " and " + b + " and " + c);
+                        console.log("The product of these is " + (a * b * c));
+                        break second;
+                    }
                 }
             }
         }
