e4da5a9d85dac72f8f3bec56c93bf911766b3ecf
Steven Davies-Cortes <stevenstdc@gmail.com>
2020-12-03 21:52:52 +0000

    fixed file error

--- Steve_Code/2020-AOC-Steve.py
+++ Steve_Code/2020-AOC-Steve.py
@@ -24,7 +24,7 @@ import pathlib
 from itertools import combinations
 import numpy as np
 
-base = pathlib.Path('/home/steve/AOC/')
+base = pathlib.Path('/home/steve/AOC/AOC2020/Steve_Code')
 raw = base / 'inputs'
 
 import os
@@ -146,7 +146,7 @@ print(countv2)
 
 # Due to the local geology, trees in this area only grow on exact integer coordinates in a grid. You make a map (your puzzle input) of the open squares (.) and trees (#) you can see. For example:
 #
-# ..##.......
+# ```..##.......
 # #...#...#..
 # .#....#..#.
 # ..#.#...#.#
@@ -157,14 +157,14 @@ print(countv2)
 # #.##...#...
 # #...##....#
 # .#..#...#.#
-#
+# ```
 # These aren't the only trees, though; due to something you read about once involving arboreal genetics and biome stability, the same pattern repeats to the right many times:
 #     
 # You start on the open square (.) in the top-left corner and need to reach the bottom (below the bottom-most row on your map).
 # The toboggan can only follow a few specific slopes (you opted for a cheaper model that prefers rational numbers); start by counting all the trees you would encounter for the slope right 3, down 1:
 # From your starting position at the top-left, check the position that is right 3 and down 1. Then, check the position that is right 3 and down 1 from there, and so on until you go past the bottom of the map.
 # The locations you'd check in the above example are marked here with O where there was an open square and X where there was a tree:
-#
+# ```
 # ..##.........##.........##.........##.........##.........##.......  --->
 # #..O#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
 # .#....X..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
@@ -176,6 +176,7 @@ print(countv2)
 # #.##...#...#.##...#...#.X#...#...#.##...#...#.##...#...#.##...#...
 # #...##....##...##....##...#X....##...##....##...##....##...##....#
 # .#..#...#.#.#..#...#.#.#..#...X.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+# ```
 #
 # In this example, traversing the map using this slope would cause you to encounter 7 trees.
 # Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees would you encounter?
@@ -186,4 +187,6 @@ fname = "q3.txt"
 file = raw / fname
 f = open(file, "r")
 
+ ls = [x.strip() for x in f.readlines()]
 
+# ls
