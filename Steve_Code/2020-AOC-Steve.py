b21021d67646b918df0abb6d0ab5d20b10a09ebf
Steven Davies-Cortes <stevenstdc@gmail.com>
2020-12-04 10:41:15 +0000

    q4 p1 comp

--- Steve_Code/2020-AOC-Steve.py
+++ Steve_Code/2020-AOC-Steve.py
@@ -23,6 +23,7 @@ import pathlib
 # import pandas as pd
 from itertools import combinations
 import numpy as np
+import re
 
 base = pathlib.Path('/home/steve/AOC/AOC2020/Steve_Code')
 raw = base / 'inputs'
@@ -207,8 +208,74 @@ def count_trees(slope_x, slope_y):
     return trees
 
 
-
 print(count_trees(3, 1))
 
 print( count_trees(1, 1) * count_trees(3, 1) * count_trees(5, 1)
     * count_trees(7, 1) * count_trees(1, 2))
+
+# ## Question 4
+
+# Due to some questionable network security, you realize you might be able to solve both of these problems at the same time.
+#
+# The automatic passport scanners are slow because they're having trouble detecting which passports have all required fields. The expected fields are as follows:
+# ```
+# byr (Birth Year)
+# iyr (Issue Year)
+# eyr (Expiration Year)
+# hgt (Height)
+# hcl (Hair Color)
+# ecl (Eye Color)
+# pid (Passport ID)
+# cid (Country ID)
+# ```
+# Passport data is validated in batch files (your puzzle input). Each passport is represented as a sequence of key:value pairs separated by spaces or newlines. Passports are separated by blank lines.
+#
+# Here is an example batch file containing four passports:
+# ```
+# ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+# byr:1937 iyr:2017 cid:147 hgt:183cm
+#
+# iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+# hcl:#cfa07d byr:1929
+#
+# hcl:#ae17e1 iyr:2013
+# eyr:2024
+# ecl:brn pid:760753108 byr:1931
+# hgt:179cm
+#
+# hcl:#cfa07d eyr:2025 pid:166559648
+# iyr:2011 ecl:brn hgt:59in
+# ```
+# The first passport is valid - all eight fields are present. The second passport is invalid - it is missing hgt (the Height field).
+# The third passport is interesting; the only missing field is cid, so it looks like data from North Pole Credentials, not a passport at all! Surely, nobody would mind if you made the system temporarily ignore missing cid fields. Treat this "passport" as valid.
+# The fourth passport is missing two fields, cid and byr. Missing cid is fine, but missing any other field is not, so this passport is invalid.
+# According to the above rules, your improved system would report 2 valid passports.
+# Count the number of valid passports - those that have all required fields. Treat cid as optional. In your batch file, how many passports are valid?
+
+# ### File read
+
+fname = "q4.txt"
+file = raw / fname
+f = open(file, "r")
+
+# ### Part 1
+
+data = f.read()
+
+passports = []
+for block in data.split('\n\n'):
+    parsed = re.findall(r'(\w+):(\S+)', block)
+    passports.append({m[0]: m[1] for m in parsed})
+
+passports
+
+# +
+required = {'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'}
+
+
+def is_valid(passport):
+    return not any(required - passport.keys())
+
+
+print(sum(map(is_valid, passports)))
+
