5661d07afc310b48d84cddd0e460a32383943b20
Chuzzy <thechuzzyguy@gmail.com>
2020-12-06 05:26:03 +0000

    Solve day 6

--- Sam_Code/06/day06.ts
+++ Sam_Code/06/day06.ts
@@ -2,5 +2,17 @@ import { readFile } from "fs";
 readFile("06/input.txt", (err, data) => {
     if (err) throw err;
 
-    const input = data.toString().split("\r\n");
+    const input = data.toString().split("\r\n\r\n");
+    const groupsOfPeople: string[][] = input.map(group => group.split("\r\n"));
+    const questionsAnswered: number[] = groupsOfPeople.map(person => new Set(person.join("")).size);
+    console.log(`The number of unique answered questions per group in total is ${questionsAnswered.reduce((total, count) => total + count, 0)}`);
+
+    const answeredByEveryoneElse = (question: string, otherAnswers: string[]): boolean => 
+        otherAnswers.every(otherAnswer => otherAnswer.includes(question));
+
+    const allQuestionsAnswered: number[] = groupsOfPeople.map(
+        group => (group[0].split("").filter(q => answeredByEveryoneElse(q, group))).length
+    );
+
+    console.log(`The number of unique answered questions answered by everyone in each group in total is ${allQuestionsAnswered.reduce((total, count) => total + count, 0)}`);
 });
