6298fddbbeb6102e617c7fe8bf923c2e93858e1b
Chuzzy <thechuzzyguy@gmail.com>
2020-12-06 05:28:49 +0000

    Create sum function to make solution self-documenting

--- Sam_Code/06/day06.ts
+++ Sam_Code/06/day06.ts
@@ -5,7 +5,10 @@ readFile("06/input.txt", (err, data) => {
     const input = data.toString().split("\r\n\r\n");
     const groupsOfPeople: string[][] = input.map(group => group.split("\r\n"));
     const questionsAnswered: number[] = groupsOfPeople.map(person => new Set(person.join("")).size);
-    console.log(`The number of unique answered questions per group in total is ${questionsAnswered.reduce((total, count) => total + count, 0)}`);
+
+    const sum = (nums: number[]): number => nums.reduce((total, count) => total + count, 0);
+
+    console.log(`The number of unique answered questions per group in total is ${sum(questionsAnswered)}`);
 
     const answeredByEveryoneElse = (question: string, otherAnswers: string[]): boolean => 
         otherAnswers.every(otherAnswer => otherAnswer.includes(question));
@@ -14,5 +17,5 @@ readFile("06/input.txt", (err, data) => {
         group => (group[0].split("").filter(q => answeredByEveryoneElse(q, group))).length
     );
 
-    console.log(`The number of unique answered questions answered by everyone in each group in total is ${allQuestionsAnswered.reduce((total, count) => total + count, 0)}`);
+    console.log(`The number of unique answered questions answered by everyone in each group in total is ${sum(allQuestionsAnswered)}`);
 });
