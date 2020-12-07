f5d7f7900e8d1bf3555446defc5539026b439913
Steven Davies-Cortes <stevenstdc@gmail.com>
2020-12-07 12:22:12 +0000

    q5 comp

--- Steve_Code/2020-AOC-Steve.py
+++ Steve_Code/2020-AOC-Steve.py
@@ -280,7 +280,7 @@ print(sum(map(is_valid, passports)))
 
 # -
 
-# ## Part 2
+# ### Part 2
 
 # +
 def is_valid(passport):
@@ -323,3 +323,68 @@ def is_valid(passport):
         return False
 
 print(sum(map(is_valid, passports)))
+# -
+
+# ## Question 5
+
+# ```
+# he first 7 characters will either be F or B; these specify exactly one of the 128 rows on the plane (numbered 0 through 127). Each letter tells you which half of a region the given seat is in. Start with the whole list of rows; the first letter indicates whether the seat is in the front (0 through 63) or the back (64 through 127). The next letter indicates which half of that region the seat is in, and so on until you're left with exactly one row.
+#
+# For example, consider just the first seven characters of FBFBBFFRLR:
+#
+# Start by considering the whole range, rows 0 through 127.
+# F means to take the lower half, keeping rows 0 through 63.
+# B means to take the upper half, keeping rows 32 through 63.
+# F means to take the lower half, keeping rows 32 through 47.
+# B means to take the upper half, keeping rows 40 through 47.
+# B keeps rows 44 through 47.
+# F keeps rows 44 through 45.
+# The final F keeps the lower of the two, row 44.
+# The last three characters will be either L or R; these specify exactly one of the 8 columns of seats on the plane (numbered 0 through 7). The same process as above proceeds again, this time with only three steps. L means to keep the lower half, while R means to keep the upper half.
+#
+# For example, consider just the last 3 characters of FBFBBFFRLR:
+#
+# Start by considering the whole range, columns 0 through 7.
+# R means to take the upper half, keeping columns 4 through 7.
+# L means to take the lower half, keeping columns 4 through 5.
+# The final R keeps the upper of the two, column 5.
+# So, decoding FBFBBFFRLR reveals that it is the seat at row 44, column 5.
+#
+# Every seat also has a unique seat ID: multiply the row by 8, then add the column. In this example, the seat has ID 44 * 8 + 5 = 357.
+#
+# Here are some other boarding passes:
+#
+# BFFFBBFRRR: row 70, column 7, seat ID 567.
+# FFFBBBFRRR: row 14, column 7, seat ID 119.
+# BBFFBBFRLL: row 102, column 4, seat ID 820.
+# As a sanity check, look through your list of boarding passes. What is the highest seat ID on a boarding pass?
+# ```
+
+# ### File read
+
+fname = "q5.txt"
+file = raw / fname
+f = open(file, "r")
+
+# ### Part 1
+
+ls = [x.strip() for x in f.readlines()]
+
+
+# +
+def binary(w):
+    w = re.sub('[FL]', '0', w)
+    w = re.sub('[BR]', '1', w)
+    return int(w, 2)
+
+print(max(map(binary, ls)))
+# -
+
+# ### Part 2
+
+all_ids = sorted(list(map(binary, ls)))
+def find_missing(lst): 
+    start = lst[0] 
+    end = lst[-1] 
+    return sorted(set(range(start, end + 1)).difference(lst))
+print(*find_missing(allids)) 
