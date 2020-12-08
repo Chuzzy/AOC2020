4be123e2979ee529c9550eb86219d55d298ead2a
Steven Davies-Cortes <stevenstdc@gmail.com>
2020-12-08 12:09:46 +0000

    q6 done also

--- Steve_Code/2020-AOC-Steve.py
+++ Steve_Code/2020-AOC-Steve.py
@@ -389,6 +389,63 @@ def find_missing(lst):
     return sorted(set(range(start, end + 1)).difference(lst))
 print(*find_missing(allids)) 
 
+# ### Day 6
+
+# The form asks a series of 26 yes-or-no questions marked a through z. All you need to do is identify the questions for which anyone in your group answers "yes". Since your group is just you, this doesn't take very long.
+#
+# However, the person sitting next to you seems to be experiencing a language barrier and asks if you can help. For each of the people in their group, you write down the questions for which they answer "yes", one per line. For example:
+# ```
+# abcx
+# abcy
+# abcz
+# ```
+# In this group, there are 6 questions to which anyone answered "yes": a, b, c, x, y, and z. (Duplicate answers to the same question don't count extra; each question counts at most once.)
+#
+# Another group asks for your help, then another, and eventually you've collected answers from every group on the plane (your puzzle input). Each group's answers are separated by a blank line, and within each group, each person's answers are on a single line. For example:
+# ```
+# abc
+#
+# a
+# b
+# c
+#
+# ab
+# ac
+#
+# a
+# a
+# a
+# a
+#
+# b
+# ```
+# This list represents answers from five groups:
+#
+# The first group contains one person who answered "yes" to 3 questions: a, b, and c.
+# The second group contains three people; combined, they answered "yes" to 3 questions: a, b, and c.
+# The third group contains two people; combined, they answered "yes" to 3 questions: a, b, and c.
+# The fourth group contains four people; combined, they answered "yes" to only 1 question, a.
+# The last group contains one person who answered "yes" to only 1 question, b.
+# In this example, the sum of these counts is 3 + 3 + 3 + 1 + 1 = 11.
+#
+# For each group, count the number of questions to which anyone answered "yes". What is the sum of those counts?
+
+# ### File read
+
+fname = "q6.txt"
+file = raw / fname
+f = open(file, "r")
+
+# ### Part 1
+
+ groups = f.read().strip().split('\n\n')
+
+print(sum(len(set(g.replace('\n', ''))) for g in groups))
+
+# ### Part 2
+
+print(sum(len(set.intersection(*map(set, g.split('\n')))) for g in groups))
+
 # ## DAY 7
 
 # ue to recent aviation regulations, many rules (your puzzle input) are being enforced about bags and their contents; bags must be color-coded and must contain specific quantities of other color-coded bags. Apparently, nobody responsible for these regulations considered how long they would take to enforce!
@@ -421,9 +478,10 @@ print(*find_missing(allids))
 
 # ### Import/File read
 
-# jupyter inline pip install because I was lazy and forgot to make a new conda env f
+# jupyter inline pip install because I was lazy and forgot to make a new conda env for this AOC
 !{sys.executable} -m pip install networkx
 
+# Makes find a predecessor in a directed tree later on a lot easier
 import networkx as nx
 
 
@@ -541,3 +599,7 @@ def run(insts, values):
 
 x, acc = run(instructions, values)
 print(acc)
+
+# ### Part 2
+
+
