022338864981e99165992a2079a5f5aa8ecd80c3
Chuzzy <thechuzzyguy@gmail.com>
2020-12-22 14:12:33 +0000

    Begin solution to day 13

--- Sam_Code/13/day13.ts
+++ Sam_Code/13/day13.ts
@@ -3,4 +3,20 @@ readFile("13/input.txt", (err, data) => {
     if (err) throw err;
 
     const input = data.toString().split("\r\n");
+    const target = parseInt(input[0]);
+    const buses = input[1].split(",").map(n => parseInt(n));
+    
+    const waitingTimes = buses.map(id => {
+        if (isNaN(id))
+            return Infinity;
+
+        return target % id;
+    });
+
+    const earliestTime = Math.min(...waitingTimes);
+    const earliestBusID = buses[waitingTimes.indexOf(earliestTime)];
+
+    console.log(`The earliest bus is the number ${earliestBusID} and you'll be waiting ${earliestTime} units of time for it.`);
+    console.log(`The product of these two numbers is ${earliestTime * earliestBusID}`);    
+
 });
