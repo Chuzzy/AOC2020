3cf3b29c031dd02a8c76a979b303f8d2725a7256
Chuzzy <thechuzzyguy@gmail.com>
2020-12-22 01:56:15 +0000

    Solve day 11 part 2

--- Sam_Code/11/day11.ts
+++ Sam_Code/11/day11.ts
@@ -44,7 +44,7 @@ readFile("11/input.txt", (err, data) => {
                     ).filter(
                         cell => cell == "#"
                     ).length;
-                
+
                 if (row[x] == "L" && numOfAdjacentOccupants == 0) {
                     result[y].push("#");
                 } else if (row[x] == "#" && numOfAdjacentOccupants >= 4) {
@@ -75,5 +75,76 @@ readFile("11/input.txt", (err, data) => {
         }
     } while (true);
 
-    console.log(`After everyone stops moving, the number of occupied seats is ${newStr.split("").filter(c => c == "#").length}`);    
+    console.log(`After everyone stops moving, the number of occupied seats is ${newStr.split("").filter(c => c == "#").length}`);
+
+    const raycastSeat = (cells: string[][], coords: [number, number], direction: [number, number]): string => {
+        const newPos: [number, number] = [coords[0] + direction[0], coords[1] + direction[1]];
+
+        if (0 > newPos[0] || newPos[0] > width - 1 || 0 > newPos[1] || newPos[1] > height - 1) {
+            return ".";
+        }
+
+        const contents = cellContents(cells, newPos);
+
+        if (contents == ".") {
+            return raycastSeat(cells, newPos, direction);
+        } else {
+            return contents;
+        }
+    };
+
+    const tick2 = (cells: string[][]): string[][] => {
+        const result: string[][] = [];
+        const cardinalDirections: [number, number][] = [
+            [-1, -1],
+            [0, -1],
+            [1, -1],
+            [1, 0],
+            [1, 1],
+            [0, 1],
+            [-1, 1],
+            [-1, 0]
+        ];
+
+        for (let y = 0; y < height; y++) {
+            result.push([]);
+            const row: string[] = cells[y];
+            for (let x = 0; x < width; x++) {
+                const numOfVisibleOccupants: number =
+                    cardinalDirections.map(
+                        direction => raycastSeat(cells, [x, y], direction)
+                    ).filter(
+                        cell => cell == "#"
+                    ).length;
+
+                if (row[x] == "L" && numOfVisibleOccupants == 0) {
+                    result[y].push("#");
+                } else if (row[x] == "#" && numOfVisibleOccupants >= 5) {
+                    result[y].push("L");
+                } else {
+                    result[y].push(row[x]);
+                }
+            }
+        }
+
+        return result;
+    };
+
+    currentGrid = startingGrid;
+    newGrid = [];
+    currStr = ""; newStr = "";
+
+    do {
+        newGrid = tick2(currentGrid);
+        currStr = currentGrid.map(row => row.join("")).join("\n");
+        newStr = newGrid.map(row => row.join("")).join("\n");
+
+        if (currStr == newStr) {
+            break;
+        } else {
+            currentGrid = newGrid;
+        }
+    } while (true);
+
+    console.log(`After everyone stops moving AGAIN, the number of occupied seats is ${newStr.split("").filter(c => c == "#").length}`);
 });
